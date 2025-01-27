d840d4812bc4dbf7abf2ba4d2226af55f41d89ab
CheaIsing <cheaising07@gmail.com>
2025-01-27 10:32:43 +0700

    m

--- app.js
+++ app.js
@@ -3,30 +3,32 @@ require("dotenv").config();
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
+const fileUpload = require("express-fileupload");
 const { checkUser, requireAuth } = require("./middlewares/auth");
 
 // API
 const apiAuth = require("./routes/api/auth");
-const apiProfile = require("./routes/api/profile")
+const apiProfile = require("./routes/api/profile");
 
 // WEB
 const webAuth = require("./routes/web/auth");
-const webEvent = require('./routes/web/event')
+const webEvent = require("./routes/web/event");
 
 const app = express();
 
 app.set("view engine", "ejs");
 
 app.use(cookieParser());
+app.use(fileUpload());
 app.use(express.static("public"));
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("*", checkUser);
 
 // API
 app.use("/api/auth", apiAuth);
-app.use("/api/profile", apiProfile)
+app.use("/api/profile", apiProfile);
 // app.use("/api/profile", )
 
 // WEB
@@ -42,4 +44,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-
