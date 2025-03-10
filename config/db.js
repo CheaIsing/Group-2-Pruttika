5e5a62e8c771487ca4504fb88a3b598f96c46f35
CheaIsing <cheaising07@gmail.com>
2025-03-10 09:26:35 +0700

    host

--- config/db.js
+++ config/db.js
@@ -2,8 +2,7 @@ const mysql = require('mysql');
 
 const conn = mysql.createConnection({
     host: process.env.HOST,
-    user: process.env.USER,
-    // user: process.env.USER_DB,
+    user: process.env.USER_DB,
     password: process.env.PASS,
     database: process.env.DB,
     port: process.env.DB_PORT || 3306
