d3b18bec3d6b9f895c2c948e1b9b1cddccd64b23
CheaIsing <cheaising07@gmail.com>
2025-03-10 08:46:12 +0700

    fix socket

--- socket/socketHelper.js
+++ socket/socketHelper.js
@@ -2,10 +2,10 @@
 function emitTicketApprovalNotification(io, buyerId, eventId, engName, eventType) {
     let engMessage, khMessage;
   
-    if (eventType === 1) { // Online event
+    if (eventType == 1) { // Online event
       engMessage = `Congratulations! Your registration for the event ${engName} has been approved. Please wait until the event day, as the organizer will send the link to join the event through a notification.`;
       khMessage = `អបអរសាទរ! ការចុះឈ្មោះរបស់អ្នកសម្រាប់ព្រឹត្តិការណ៍ ${engName} នេះត្រូវបានអនុម័តជោគជ័យ។ សូមរង់ចាំរហូតដល់ថ្ងៃព្រឹត្តិការណ៍មកដល់ ព្រោះអ្នករៀបចំនឹងផ្ញើតំណភ្ជាប់ដើម្បីចូលរួមកម្មវិធីតាមរយៈការជូនដំណឹង។`;
-    } else if (eventType === 2) { // Offline event
+    } else if (eventType == 2) { // Offline event
       engMessage = `Congratulations! Your ticket for ${engName} has been successfully approved. Enjoy the event! Check your details here.`;
       khMessage = `អបអរសាទរ! សំបុត្ររបស់អ្នកសម្រាប់កម្មវិធី ${engName} ត្រូវបានអនុម័តដោយជោគជ័យ។ សូមរីករាយជាមួយព្រឹត្តិការណ៍នេះ! ពិនិត្យព័ត៌មានលម្អិតរបស់អ្នកនៅទីនេះ។`;
     } else {
@@ -13,6 +13,9 @@ function emitTicketApprovalNotification(io, buyerId, eventId, engName, eventType
       console.error(`Unknown event type: ${eventType}`);
       return; // Or throw an error
     }
+
+    console.log("approve ticket run here");
+    
   
     io.to(buyerId).emit('notification', {
       event_id: eventId,
